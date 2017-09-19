a26a31594b61f6d30ecaa82396879fab89270d62
LumberMan1301 <marianomm@rocketmail.com>
2017-09-19 16:21:33 -0600

    json Simple

--- src/Logica/ArchivosTXT/ArchivosTXT.java
+++
@@ -1,7 +0,0 @@
-package Logica.ArchivosTXT;
-
-/**
- * Created by 13mariano on 19 sep 2017.
- */
-public class ArchivosTXT {
-}
--- src/Logica/ObjetoJSON/ObjetoJSON.java
+++ src/Logica/ObjetoJSON/ObjetoJSON.java
@@ -4,4 +4,19 @@ package Logica.ObjetoJSON;
  * Created by 13mariano on 18 sep 2017.
  */
 public class ObjetoJSON {
+
+
+
+    public void crearJSON (String nombre){
+        this.crearJSONP(nombre);
+    }
+
+    private void crearJSONP (String nombre){
+
+        JSONObject nombre = new
+
+    }
+
+
+
 }
